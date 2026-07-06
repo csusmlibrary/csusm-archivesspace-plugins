ccb56a47f20119a6373884cb6e9238b8dd4cb1db
Khuong Vu <kvu@csusm.edu>
2026-07-06 13:39:13 -0700

    (a) Use magnifying glass for New Search.  (b) Lyrasis should be able to add link for logo.

--- csusm_branding/public/assets/javascripts/csusm.js
+++ csusm_branding/public/assets/javascripts/csusm.js
@@ -1,9 +1,7 @@
 jQuery(document).ready(function(){
         var pathname = window.location.pathname;
         var archive_obj = window.location.href;
-        jQuery("#navigation .navbar #collapsemenu ul").append("<li class='nav-item p-2'><a class='nav-link' href='/'>New Search</a></li>");
         jQuery("#navigation .navbar #collapsemenu ul").after("<ul class='nav navbar-nav navbar-right'><li><a href='https://archives.csusm.edu'>Special Collections Home Page</a></li></ul>");
-        jQuery("#header img[src='/assets/csusm.png']").closest("a").attr("href", "https://library.csusm.edu/");
         jQuery("#request_sub").replaceWith("<a href='https://archives.csusm.edu/request?ref_url=https://archivessearch.csusm.edu"+pathname+"' class='btn page_action request  btn-default' target='_blank'><i class='fa fa-bullhorn fa-3x'></i><br/>Request<span class='sr-only'>(opens in a new window)</span></a>");
 	// 07/03/26 Khuong: A number of lines were deleted, because they are no longer
 	// applicable; the UI element(s) have changed because of the data or because
